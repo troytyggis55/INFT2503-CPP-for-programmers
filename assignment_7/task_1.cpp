4534f6c10c6fa87527dcdbf5fed941f746d75545
Trygve Jørgensen <trygveabjo@gmail.com>
2024-07-20 17:39:08 +0200

    imported fraction example files

---
+++ assignment_7/task_1.cpp
@@ -0,0 +1,43 @@
+#include "fraction.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(const string &text, const Fraction &broek) {
+  cout << text << broek.numerator << " / " << broek.denominator << endl;
+}
+
+int main() {
+  Fraction a(10, 20);
+  Fraction b(3, 4);
+  Fraction c;
+  c.set(5);
+  Fraction d = a / b;
+
+  print("a = ", a);
+  print("b = ", b);
+  print("c = ", c);
+  print("d = ", d);
+
+  b += a;
+  ++c;
+  d *= d;
+
+  print("b = ", b);
+  print("c = ", c);
+  print("d = ", d);
+
+  c = a + b - d * a;
+  c = -c;
+
+  print("c = ", c);
+
+  if (a + b != c + d)
+    cout << "a + b != c + d" << endl;
+  else
+    cout << " a + b == c + d" << endl;
+  while (b > a)
+    b -= a;
+  print("b = ", b);
+}
