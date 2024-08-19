1986535a2fbb3ac4c8068d53bb27613ed2c6053a
Trygve Jørgensen <trygveabjo@gmail.com>
2024-08-19 16:48:29 +0200

    prints template instead of generic

--- assignment_8/task_1.cpp
+++ assignment_8/task_1.cpp
@@ -4,23 +4,36 @@ using namespace std;
 
 template <typename Type>
 bool equal(Type a, Type b) {
-    cout << "Generic" << endl;
+    cout << "Template ";
     return a == b;
 }
 
 bool equal(double a, double b) {
-    cout << "Double" << endl;
+    cout << "Double ";
     return a - b < 0.01 && a - b > -0.01;
 }
 
 int main() {
     int int_a = 1;
     int int_b = 2;
-    cout << equal(int_a, int_b) << endl;
+    if (equal(int_a, int_b)) {
+        cout << "Equal" << endl;
+    } else {
+        cout << "Not equal" << endl;
+    }
 
     double double_a = 1.0;
     double double_b = 1.005;
     double double_c = 1.02;
-    cout << equal(double_a, double_b) << endl;
-    cout << equal(double_a, double_c) << endl;
+    if (equal(double_a, double_b)) {
+        cout << "Equal" << endl;
+    } else {
+        cout << "Not equal" << endl;
+    }
+
+    if (equal(double_a, double_c)) {
+        cout << "Equal" << endl;
+    } else {
+        cout << "Not equal" << endl;
+    }
 }
